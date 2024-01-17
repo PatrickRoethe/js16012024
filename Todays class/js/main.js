c65b046369d3d6648f93db23f4f2b2f91f9913c1
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2024-01-17 23:23:09 +0100

    Update main.js
    defined an array of category names for the H1

--- Todays class/js/main.js
+++ Todays class/js/main.js
@@ -10,8 +10,8 @@ let categoriesHTML = "";
 // SNORRE comment: Here is a function that checks how many categories there are in the data.
 function countCategories(data) {
   const uniqueCategories = new Set();
-  
-  data.forEach(item => {
+
+  data.forEach((item) => {
     uniqueCategories.add(item.category);
   });
 
@@ -21,39 +21,45 @@ function countCategories(data) {
 // SNORRE comment: Here we are using the function to create a variable that contains how many categories there are in "productsData"
 const numberOfCategories = countCategories(productsData);
 
+// SNORRE comment: Define an array of category names
+const categoryNames = ["Apple", "Samsung", "Nokia"]; // Add more names as needed
+
 // Step 4: Loop through each category and generate HTML markup
 // SNORRE comment: Here we are using the "numberOfCategories" so that the for-loop will auto adjust to the data (if there are 3 categories in the data file, it will loop 3 times)
-for (let categoryIndex = 1; categoryIndex <= numberOfCategories; categoryIndex++) {
+for (
+  let categoryIndex = 0;
+  categoryIndex < numberOfCategories;
+  categoryIndex++
+) {
   // Generate HTML markup for each product in the category
   let productsHTML = "";
 
   for (let i = 0; i < productsData.length; i++) {
-
     // SNORRE comment: So inside this for-loop we want to only add the products that are in the current category.
     // In the if statement below we are comparing the product category to the categoryIndex to ensure we only include the products we want in each category
-    if (productsData[i].category === categoryIndex) {
+    if (productsData[i].category === categoryIndex + 1) {
       productsHTML += `
-      <div class="product">
-        <img alt="random photo" src="https://picsum.photos/200" />
-        <h3>${productsData[i].name}</h3>
-        <h4>NOK ${productsData[i].price}</h4>
-        <span class="heart-container">
-          <svg class="heart"
-            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-            fill="none" stroke="#000000"
-            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-          </svg>
-        </span>
-      </div>
-    `;
+        <div class="product">
+          <img alt="random photo" src="https://picsum.photos/200" />
+          <h3>${productsData[i].name}</h3>
+          <h4>NOK ${productsData[i].price}</h4>
+          <span class="heart-container">
+            <svg class="heart"
+              xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+              fill="none" stroke="#000000"
+              stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+            </svg>
+          </span>
+        </div>
+      `;
     }
   }
 
   // Generate HTML markup for the category with the products
   categoriesHTML += `
     <section class="category">
-      <h1>Category: ${categoryIndex}</h1>
+      <h1>${categoryNames[categoryIndex]}</h1>
       <div class="products-container">
         ${productsHTML}
       </div>
