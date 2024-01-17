2d06df648a446636e519430eb26dcae472d3c510
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2024-01-17 18:07:54 +0100

    update html and css

--- Todays class/js/data/products.js
+++ Todays class/js/data/products.js
@@ -1,34 +1,25 @@
 export default [
   {
     id: 1,
-    name: "iphone 15",
+    name: "Iphone 15",
     price: 10000,
   },
 
   {
     id: 2,
-    name: "iphone 14",
+    name: "Iphone 14",
     price: 10001,
   },
 
   {
     id: 3,
-    name: "iphone 13",
+    name: "Iphone 13",
     price: 10002,
   },
 
   {
     id: 4,
-    name: "iphone 12",
+    name: "Iphone 12",
     price: 10003,
   },
-
-  {
-    id: 5,
-    name: "iphone 11",
-    price: 10004,
-  },
 ];
-
-let name = "thomas";
-export { name };
