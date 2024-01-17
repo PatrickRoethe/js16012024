4111c7fa8c9d6c868d6048dec027b182c9dc27b7
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2024-01-17 21:14:57 +0100

    updated in css and product.js
    added categories to products.js

--- Todays class/js/data/products.js
+++ Todays class/js/data/products.js
@@ -3,23 +3,27 @@ export default [
     id: 1,
     name: "Iphone 15",
     price: 10000,
+    category: 1,
   },
 
   {
     id: 2,
     name: "Iphone 14",
     price: 10001,
+    category: 1,
   },
 
   {
     id: 3,
     name: "Iphone 13",
     price: 10002,
+    category: 1,
   },
 
   {
     id: 4,
     name: "Iphone 12",
     price: 10003,
+    category: 1,
   },
 ];
